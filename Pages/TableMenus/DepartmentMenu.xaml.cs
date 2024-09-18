067f125361209166fce2e29633c72e29d35d1a0b
Qwib-Kwib <marcpantinos@gmail.com>
2024-09-18 18:20:56 +0800

    test

--- Pages/TableMenus/DepartmentMenu.xaml.cs
+++ Pages/TableMenus/DepartmentMenu.xaml.cs
@@ -19,6 +19,7 @@ using MySql.Data.MySqlClient;
 
 namespace Info_module.Pages.TableMenus
 {
+    /// newwwwwwwwwwwwwwwwwwwwww
     /// <summary>
     /// Interaction logic for DepartmentMenu.xaml
     /// </summary>
